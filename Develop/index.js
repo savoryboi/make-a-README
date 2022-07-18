4854f7deaf63610d492dccafc697cb47bd58970d
savoryboi <andbjerk@yahoo.com>
2022-07-18 17:09:12 -0500

    spacing fixes

--- Develop/index.js
+++ Develop/index.js
@@ -45,7 +45,7 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(answers) {
     const { title, about, install, usage, license, credits, email } = answers;
-    
+
 if(license === 'OPEN SOURCE'){
     var badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
     var licenseChoice = `
@@ -95,8 +95,10 @@ ${usage}
 
 ## Licensing
 ${licenseChoice}
+
 ## Credits
 ${credits}
+
 ## Contact
 ${email}  
 [GitHub](https://github.com/savoryboi)
